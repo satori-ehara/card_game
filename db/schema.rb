327c3af5bcd820609fc61c76ffa08bbcb07bcaf2
satori-ehara <satori.0701191@gmail.com>
2020-07-31 17:16:39 +0900

    大まかなゲームのビュー完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_033612) do
+ActiveRecord::Schema.define(version: 2020_07_31_070745) do
+
+  create_table "games", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
