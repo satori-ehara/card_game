587276359cad31f6bb5f0b19ec700d5443638418
satori-ehara <satori.0701191@gmail.com>
2020-08-04 14:48:51 +0900

    コントローラーにてデッキ内の数値を決めるように設定

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,20 @@
 class GamesController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    create_deck
+
+    binding.pry
+  end
+
+  private
+  def create_deck
+    params[:deck] = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,10].shuffle
+    last_card = params[:deck].pop
+    params[:deck] = [params[:deck],[last_card]]
+  end
 end
--- db/migrate/20200731070745_create_games.rb
+++ db/migrate/20200731070745_create_games.rb
@@ -1,6 +1,12 @@
 class CreateGames < ActiveRecord::Migration[6.0]
   def change
     create_table :games do |t|
+      t.string :condition
+      t.string :deck
+      t.integer :field_card
+      t.string :turn
+      t.string :action
+      t.string :turn_count
 
       t.timestamps
     end
