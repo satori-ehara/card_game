fa7b28b063fd8b4634a152ab9d7a2712f49b079b
satori-ehara <satori.0701191@gmail.com>
2020-08-04 14:14:01 +0900

    ゲームのルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
   resources :groups,only: [:index, :new, :create] do
-    resources :games, only: [:index] 
+    resources :games, only: [:index, :new, :create] 
   end
 end
